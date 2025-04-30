5d6e7036086321a27095c572ddb61f80685cbdca
Caity <caiwu993@student.liu.se>
2025-04-30 16:39:14 +0200

    moved and added files
    moved json
    created fetchdatafromjson

---
+++ my-react-app/src/fetchdata.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+
+function FetchData() {
+  const [nodes, setNodes] = useState([])
+
+  useEffect(() => {
+    fetch('/MC1_graph.json')  // laddas från public/
+      .then(response => response.json())
+      .then(data => setNodes(data.nodes))
+      .catch(error => console.error("Fel vid laddning:", error))
+  }, []) // tom array = bara första gången
+
+  return (
+    <div>
+      <h2>Persons</h2>
+      {nodes.
+      filter(node => node["Node Type"] === "Person")
+      .map((node, i) => (
+        <div key={node.id}>
+          <p><strong>{node["Node Type"]}</strong> - {node.name}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default FetchData
